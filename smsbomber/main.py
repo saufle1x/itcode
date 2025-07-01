c9d608fd891f8bd8db0d7484fcd417ffbb8b84b5
saufle1x <illyakuzenko1@gmail.com>
2025-07-01 20:56:35 +0300

    Update main.py

--- smsbomber/main.py
+++ smsbomber/main.py
@@ -1,4 +1,11 @@
 import sms
-number = '+7 999 999 99 99'
-while True:
+import time  
+
+number = '+79999999999'  
+counter = 0
+max_messages = 3  
+
+while counter < max_messages:
     sms.send(number, 'ХПХХППХХП')
+    counter += 1
+    time.sleep(1)
